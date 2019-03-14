5ae79c1f5ed79bbdae5f0f73448fc201ae7d88c2
fairymon <42788160+fairymon@users.noreply.github.com>
2019-03-14 01:32:11 -0400

    Add files via upload

---
+++ studio/project_wine/index.js
@@ -0,0 +1,10 @@
+$('body').onClick(function(){
+    $('#fill').addClass('animateFill');
+		$('.wave').addClass('animateWave');
+});
+
+window.addEventListener('click', function(e) {
+var audio = document.getElementById('PoppingSound');
+audio.oncanplaythrough = function(){
+
+)}
