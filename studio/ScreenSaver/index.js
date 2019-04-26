c45c73ae00bd47eb5100e9ec152a56e0ae0cee27
fairymon <42788160+fairymon@users.noreply.github.com>
2019-04-25 23:41:16 -0400

    Add files via upload

--- studio/ScreenSaver/index.js
+++ studio/ScreenSaver/index.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
 
+//  $('#container').ripples({
+//    resolution: 512,
+//    	dropRadius: 20,
+//    	perturbance: 0.04,
+//  });
+
+
 
   //get document size
   var width = $(document).width ();
@@ -7,71 +14,61 @@ $(document).ready(function(){
   var fishWidth = $(".fish").width();
   var fishHeight = $(".fish").height();
 
+
   //speed and direction of animation
-  var speedX = 4;
-  var speedY = 4;
+  var speedY = 1;
 
   //variables to move class
   var positionLeft = 0;
   var positionTop = 0;
 
-  //60frames per second
-  function step(){
-    //update css variables
-    positionLeft += speedX;
-    positionTop += speedY;
+//make an array of colors
 
-    //change speed and direction variable if the ball hits the edge of the screen
-    if( positionLeft > width) {
-      speedX = speedX * -1;
-    }
-    if( positionLeft < 0){
-      speedX = speedX * -1;
-    }
-    if (positionTop > height){
-      positionTop = 0;
-
-    }
-    if (positionTop < 0 ){
-      speedY = speedY * -1;
-    }
-
-    //edit class
-    $('.fish').css("left", positionLeft)
-    $('.fish').css("top", positionTop)
-
-    window.requestAnimationFrame(step);
-}
-    window.requestAnimationFrame(step);
-//end document ready function
-
-
-
-
+///
 
 setInterval(function(){
   //create random number
   var randomWidth = Math.random() * width - fishWidth;
   var randomHeight = Math.random() * height - fishHeight;
 
+
+
+
   //make it css ready
   var leftCss = Math.floor(randomWidth) + "px"
   var topCss = Math.floor(randomHeight) + "px"
 
 //new fish
-  $(".fish").last().clone().appendTo("#container");
-
+  $(".fish").last().clone().appendTo("body");
 
   //change the css
   $(".fish").last().css("left", leftCss)
-  $(".fish").last().css("top", topCss)
+  $(".fish").last().css("top", '-200px')
+
+
+//random color
+//pick a random color from array and replace blue
+
+
+var colors = ['#f1624e',  '#fdc0cc', '#ffecda'];
+var randomNumber = Math.random()*colors.length;
+var randomColor = colors[Math.floor(randomNumber)];
+
+
+
+$('#body', '.fish').last().css('background-color', randomColor);
+
 
   //console.log($(".fish").length)
   //$(".fish").first().remove()
 
 
+},5000)
 
-},3000)
+setInterval(function(){
+
+  $(".fish").first().remove();
+}, 8000)
 
 
 
